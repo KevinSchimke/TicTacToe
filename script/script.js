f26782e94207a8c68cf68b02aa486b6e3d332d2b
KevinSchimke <99587213+KevinSchimke@users.noreply.github.com>
2022-05-11 12:12:42 +0100

    restart game

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ let fields = [];
 let gameover = false;
 let timeoutGameover = 1000;
 let currentPlayer = 'cross';
+let drawCount = 0;
 
 function matchfield(ID) {
     if (!fields[ID] && !gameover) {
@@ -29,11 +30,14 @@ function draw() {
             document.getElementById('cross_' + i).classList.remove('d-none');
         }
     }
+    drawCount++;
 }
 
 function checkWinner() {
     let winner;
-
+    if (drawCount == 9){
+        winner = true;
+    }
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
         document.getElementById('line_0').style.transform = 'scale(1)'
@@ -46,7 +50,6 @@ function checkWinner() {
         winner = fields[6];
         document.getElementById('line_2').style.transform = 'scale(1)'
     }
-
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
         document.getElementById('line_3').style.transform = 'rotate(90deg) scale(1)'
@@ -59,7 +62,6 @@ function checkWinner() {
         winner = fields[2];
         document.getElementById('line_5').style.transform = 'rotate(90deg) scale(1)'
     }
-
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
         document.getElementById('line_6').style.transform = 'rotate(45deg) scale(1)'
@@ -68,11 +70,26 @@ function checkWinner() {
         winner = fields[2];
         document.getElementById('line_7').style.transform = 'rotate(-45deg) scale(1)'
     }
-
     if (!!winner) {
         gameover = true;
         setTimeout(function() {
             document.getElementById('game_over').classList.remove('d-none');
+            document.getElementById('restart').classList.remove('d-none');
         }, timeoutGameover);
+    }
+}
+
+function restart() {
+    gameover = false;
+    drawCount = 0;
+    fields = [];
+    document.getElementById('game_over').classList.add('d-none');
+    document.getElementById('restart').classList.add('d-none');
+    for (let index = 0; index < 8; index++) {
+        document.getElementById('line_'+index).style.transform = 'scale(0)'
+    }
+    for (let index = 0; index < 9; index++) {
+        document.getElementById('circle_'+index).classList.add('d-none');
+        document.getElementById('cross_'+index).classList.add('d-none');
     }
 }
